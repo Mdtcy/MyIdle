d6f9a100a49a9a69414b5c3ee1a9fc48a95e6541
Mdtcy <tktetb@163.com>
2022-10-25 21:42:34 +0800

    changed

---
+++ Idle/Assets/Configs/Assets/Class/ProfessionConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using HM.GameBase;
+
+namespace NewLife.Config
+{
+    [System.Serializable]
+    [CreateAssetMenu(fileName = "Profession", menuName = "Config/Profession")]
+    public class ProfessionConfig : BaseConfig
+    {
+        // Id和Name在BaseConfig已有定义，不需要重复定义
+        // 其他类型请自行定义↓↓↓
+    }
+}
