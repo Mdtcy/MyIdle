bb73577ae0733485339ac5d8c081cae2e085d960
Mdtcy <tktetb@163.com>
2022-10-29 18:53:21 +0800

    增加场景切换下拉框

--- Idle/Assets/Editor/CustomToolBar/SceneSwitcher/SceneSwitcher.cs
+++ Idle/Assets/Editor/CustomToolBar/SceneSwitcher/SceneSwitcher.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -24,25 +27,137 @@ namespace UnityToolbarExtender.Examples
 	[InitializeOnLoad]
 	public class SceneSwitchLeftButton
 	{
+		class SceneData
+		{
+			public string     path;
+			public GUIContent popupDisplay;
+		}
+
+		public string NameInList => "[Dropdown] Scene selection";
+
+		static bool showSceneFolder = true;
+
+		static SceneData[] scenesPopupDisplay;
+		static string[]    scenesPath;
+		static string[]    scenesBuildPath;
+		static int                selectedSceneIndex;
+
 		static SceneSwitchLeftButton()
 		{
 			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+			RefreshScenesList();
 		}
 
 		static void OnToolbarGUI()
 		{
+
 			GUILayout.FlexibleSpace();
 
-			if(GUILayout.Button(new GUIContent("1", "Start Scene 1"), ToolbarStyles.commandButtonStyle))
+			selectedSceneIndex = EditorGUILayout.Popup(selectedSceneIndex,
+			                                           scenesPopupDisplay.Select(e => e.popupDisplay).ToArray(),
+			                                           GUILayout.Width(100));
+
+			if (GUI.changed && 0 <= selectedSceneIndex && selectedSceneIndex < scenesPopupDisplay.Length)
 			{
-				EditorSceneManager.OpenScene("Assets/Scenes/Menu.unity");
+				if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+				{
+					foreach (var scenePath in scenesPath)
+					{
+						if ((scenePath) == scenesPopupDisplay[selectedSceneIndex].path)
+						{
+							EditorSceneManager.OpenScene(scenePath);
+
+							break;
+						}
+					}
+				}
 			}
 
-			if(GUILayout.Button(new GUIContent("2", "Start Scene 2"), ToolbarStyles.commandButtonStyle))
-			{
-			    EditorSceneManager.OpenScene("Assets/Scenes/Game.unity");
-			}
 		}
+
+		static void RefreshScenesList()
+		{
+			List<SceneData> toDisplay = new List<SceneData>();
+
+			selectedSceneIndex = -1;
+
+			scenesBuildPath = EditorBuildSettings.scenes.Select(s => s.path).ToArray();
+
+			string[] sceneGuids = AssetDatabase.FindAssets("t:scene", new string[] {"Assets"});
+			scenesPath = new string[sceneGuids.Length];
+
+			for (int i = 0; i < scenesPath.Length; ++i)
+			{
+				scenesPath[i] = AssetDatabase.GUIDToAssetPath(sceneGuids[i]);
+			}
+
+			Scene activeScene = SceneManager.GetActiveScene();
+			int   usedIds     = scenesBuildPath.Length;
+
+			for (int i = 0; i < scenesBuildPath.Length; ++i)
+			{
+				string name = GetSceneName(scenesBuildPath[i]);
+
+				if (selectedSceneIndex == -1 && GetSceneName(name) == activeScene.name)
+					selectedSceneIndex = i;
+
+				GUIContent content =
+					new GUIContent(name, EditorGUIUtility.Load("BuildSettings.Editor.Small") as Texture, "Open scene");
+
+				toDisplay.Add(new SceneData()
+				{
+					path         = scenesBuildPath[i],
+					popupDisplay = content,
+				});
+			}
+
+			toDisplay.Add(new SceneData()
+			{
+				path         = "\0",
+				popupDisplay = new GUIContent("\0"),
+			});
+			++usedIds;
+
+			for (int i = 0; i < scenesPath.Length; ++i)
+			{
+				if (scenesBuildPath.Contains(scenesPath[i]))
+					continue;
+				string name;
+
+				if (showSceneFolder)
+				{
+					string folderName = Path.GetFileName(Path.GetDirectoryName(scenesPath[i]));
+					name = $"{folderName}/{GetSceneName(scenesPath[i])}";
+				}
+				else
+				{
+					name = GetSceneName(scenesPath[i]);
+				}
+
+				if (selectedSceneIndex == -1 && name == activeScene.name)
+					selectedSceneIndex = usedIds;
+
+				GUIContent content = new GUIContent(name, "Open scene");
+
+				toDisplay.Add(new SceneData()
+				{
+					path         = scenesPath[i],
+					popupDisplay = content,
+				});
+
+				++usedIds;
+			}
+
+			scenesPopupDisplay = toDisplay.ToArray();
+		}
+
+		static string GetSceneName(string path)
+		{
+			path = path.Replace(".unity", "");
+
+			return Path.GetFileName(path);
+		}
+
 	}
 
 	static class SceneHelper
