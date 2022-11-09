c633a1e61d4daeb1af470d7e8651bb6854769b1c
Mdtcy <tktetb@163.com>
2022-11-09 21:39:31 +0800

    添加一个基类用于辅助VisualScriptStateMachine

---
+++ Idle/Assets/Scripts/VisualStateSuport/StateBase.cs
@@ -0,0 +1,43 @@
+/**
+ * @author BoLuo
+ * @email [ tktetb@163.com ]
+ * @create date  2022年11月9日
+ * @modify date 2022年11月9日
+ * @desc [任务组UI]
+ */
+
+#pragma warning disable 0649 
+using UnityEngine; 
+
+namespace VisualStateSuport
+{
+    public class StateBase : MonoBehaviour
+    {
+        
+
+        #region FIELDS
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region PUBLIC METHODS
+
+        #endregion
+
+        #region PROTECTED METHODS
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        #endregion
+
+        #region STATIC METHODS
+
+        #endregion
+    }
+}
+#pragma warning restore 0649
