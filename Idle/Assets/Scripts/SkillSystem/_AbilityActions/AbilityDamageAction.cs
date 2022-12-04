c2381a0824834175ba9c8c0da5d6e910ab691cda
Mdtcy <tktetb@163.com>
2022-12-04 15:07:16 +0800

    Revert "删除SkillSystem"
    This reverts commit 1894f94633415751bfb00a5899568ca061faad22.

---
+++ Idle/Assets/Scripts/SkillSystem/_AbilityActions/AbilityDamageAction.cs
@@ -0,0 +1,53 @@
+﻿public enum DamageType
+{
+    None,
+    Physical,
+    Poison,
+}
+
+public struct DamageInfo
+{
+    public DamageType DamageType;
+    public float Damage;
+
+    public DamageInfo(DamageType damageType, float damage)
+    {
+        DamageType = damageType;
+        Damage = damage;
+    }
+}
+
+public class AbilityDamageAction : AbilityActionBase
+{
+    public DamageInfo DamageInfo { get; private set; }
+
+    public AbilityDamageAction(string name, ICombatUnit caster, DamageInfo damageInfo) 
+        : base(name, caster)
+    {
+        DamageInfo = damageInfo;
+    }
+
+    protected override void Invoke()
+    {
+        DealDamageToTargets();
+    }
+
+    void DealDamageToTargets()
+    {
+        TargetUnits.ForEach(val => DealDamageToTarget(val));
+    }
+
+    void DealDamageToTarget(ICombatUnit combatUnit)
+    {
+        DamageInfo decoratedDamageInfo = ((IDamageDealerCombatUnit)Caster).DealDamage(DamageInfo, combatUnit);
+
+        DamageInfo dealtDamageInfo = ((IHealthOwnerCombatUnit)combatUnit).TakeDamage(decoratedDamageInfo, Caster);
+
+        if (dealtDamageInfo.Damage == 0)
+            return;
+
+        ((IDamageDealerCombatUnit)Caster).DealtDamage(dealtDamageInfo, combatUnit);
+
+        FireOnActionEnded();
+    }
+}
---
+++ Idle/Assets/Scripts/SkillSystem/_AbilityEvents/OnCollisionEnterAbilityEvent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+public class OnCollisionEnterAbilityEvent : AbilityEventBase
+{
+    AbilityBase _bindedAbility;
+
+    InteractableCollider _targetInteractableCollider;
+
+    public OnCollisionEnterAbilityEvent(InteractableCollider targetInteractableCollider, AbilityBase bindedAbility, AbilityActionBase bindedAction)
+        : base(bindedAction)
+    {
+        _targetInteractableCollider = targetInteractableCollider;
+
+        _bindedAbility = bindedAbility;
+
+        Bind();
+    }
+
+    protected override void Bind()
+    {
+        _bindedAbility.OnAbilityExecuted += OnAbilityExecuted;
+        _targetInteractableCollider.OnTargetEntered += OnTargetEntered;
+
+        CheckInitialTriggger();
+    }
+
+    protected override void Unbind()
+    {
+        _bindedAbility.OnAbilityExecuted -= OnAbilityExecuted;
+        _targetInteractableCollider.OnTargetEntered -= OnTargetEntered;
+
+        TriggerActionList.Clear();
+    }
+
+    void CheckInitialTriggger()
+    {
+        List<ICombatUnit> targets = _targetInteractableCollider.Targets;
+
+        foreach (ICombatUnit cUnit in targets)
+            OnTargetEntered(cUnit);
+    }
+
+    private void OnTargetEntered(ICombatUnit unit)
+    {
+        Trigger();
+    }
+
+    void OnAbilityExecuted()
+    {
+        Unbind();
+    }
+}
