089c3bdb166fb5d6983630212a684fa9b9894777
Mdtcy <tktetb@163.com>
2023-03-02 20:46:49 +0800

    1

--- Idle/Assets/Scripts/Common/DrawCircle.cs
+++ Idle/Assets/Scripts/Common/DrawCircle.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class DrawCircle : MonoBehaviour
 {
+    public bool  Enable;
+
     public float ridius = 1;
 
     private void OnDrawGizmos()
     {
+        if (!Enable)
+        {
+            return;
+        }
+
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, ridius);
     }
